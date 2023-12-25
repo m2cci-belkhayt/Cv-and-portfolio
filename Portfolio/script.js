f8876ed94e8a6a32d41d26ccbbadbdbcfe786631
Taha Belkhayate <69703213+m2cci-belkhayt@users.noreply.github.com>
2023-12-25 09:02:09 +0100

    my cv and portfolio

---
+++ Portfolio/script.js
@@ -0,0 +1,7 @@
+const menuBtn = document.querySelector('.menu-btn');
+const menuLinks = document.querySelector('.menu-links');
+
+menuBtn.addEventListener('click', () => {
+    //menuLinks.classList.toggle('active');
+  //menuLinks.style.display = menuLinks.style.display === 'none' ? 'flex' : 'none';
+});
